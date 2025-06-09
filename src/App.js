fba4f7a279c3bd7222120a16b9612cd255574d67
johnstringer-1111 <john.h.stringer@gmail.com>
2025-06-09 17:19:48 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FocusedInspiredActionApp from './FocusedInspiredActionApp';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <FocusedInspiredActionApp />
+    </div>
+  );
+}
+
+export default App;
